589658eef5bba21452cf8cf8f69cce8bf2481a94
Nirmal Raj <42131933+nirmalraj2000@users.noreply.github.com>
2023-02-22 21:32:21 +0530

    Commonvalidator migrated to variable of functions

--- Common/common_validator.dart
+++ Common/common_validator.dart
@@ -1,98 +1,93 @@
 import 'package:email_validator/email_validator.dart';
 
 class CoommonValidator {
-  static String? Function(String? value) plainTextValidator() {
-    return (String? value) {
-      if (value == "") {
-        return "Enter the Data";
-      }
+  static String? Function(String? value) plainTextValidator = (String? value) {
+    if (value == "") {
+      return "Enter the Data";
+    }
+    return null;
+  };
+
+  static String? Function(String? value) phoneNumberValidator =
+      (String? value) {
+    if (value == "") {
+      return "Enter the Phone Number";
+    } else if (value!.length < 10 || value.length > 10) {
+      return "Enter a Valid Phone Number";
+    }
+
+    try {
+      int.parse(value);
+    } catch (e) {
+      return "Invalid Phone Number";
+    }
+    return null;
+  };
+
+  static String? Function(String? value) ageValidator = (String? value) {
+    if (value == "") {
+      return "Enter the Age";
+    }
+
+    try {
+      int.parse(value!);
+    } catch (e) {
+      return "Invalid Age";
+    }
+    return null;
+  };
+
+  static String? Function(String? value) pincodeValidator = (String? value) {
+    if (value == "") {
+      return "Enter the Pincode";
+    } else if (value!.length < 6) {
+      return "Invalid Pincode";
+    }
+
+    try {
+      int.parse(value);
+    } catch (e) {
+      return "Invalid Pincode";
+    }
+    return null;
+  };
+
+  static String? Function(String? value) nonMandatoryPhoneNumberValidator =
+      (String? value) {
+    if (value == "") {
       return null;
-    };
-  }
+    } else if (value!.length < 10 || value.length > 10) {
+      return "Enter a Valid Phone Number";
+    }
 
-  static String? Function(String? value) phoneNumberValidator() {
-    return (String? value) {
-      if (value == "") {
-        return "Enter the Phone Number";
-      } else if (value!.length < 10 || value.length > 10) {
-        return "Enter a Valid Phone Number";
-      }
+    try {
+      int.parse(value);
+    } catch (e) {
+      return "Invalid Phone Number";
+    }
+    return null;
+  };
 
-      try {
-        int.parse(value);
-      } catch (e) {
-        return "Invalid Phone Number";
-      }
-      return null;
-    };
-  }
-
-  static String? Function(String? value) ageValidator() {
-    return (String? value) {
-      if (value == "") {
-        return "Enter the Age";
-      }
-
-      try {
-        int.parse(value!);
-      } catch (e) {
-        return "Invalid Age";
-      }
-      return null;
-    };
-  }
-
-  static String? Function(String? value) pincodeValidator() {
-    return (String? value) {
-      if (value == "") {
-        return "Enter the Pincode";
-      } else if (value!.length < 6) {
-        return "Invalid Pincode";
-      }
-
-      try {
-        int.parse(value);
-      } catch (e) {
-        return "Invalid Pincode";
-      }
-      return null;
-    };
-  }
-
-  static String? Function(String? value) nonMandatoryPhoneNumberValidator() {
-    return (String? value) {
-      if (value == "") {
+  static String? Function(String? value) emailValidator = (String? value) {
+    if (value?.isNotEmpty == true) {
+      if (EmailValidator.validate(value!) == true) {
         return null;
-      } else if (value!.length < 10 || value.length > 10) {
-        return "Enter a Valid Phone Number";
+      } else {
+        return "Enter a valid email ID";
       }
+    }
+    return "Enter the email ID";
+  };
 
-      try {
-        int.parse(value);
-      } catch (e) {
-        return "Invalid Phone Number";
-      }
-      return null;
-    };
-  }
-
-  static String? Function(String? value) emailValidator() {
-    return (String? value) {
-      if (!EmailValidator.validate(value!)) {
-        return "Enter a Valid Email";
-      }
-      return null;
-    };
-  }
-
-  static String? Function(String? value) nonMandatoryEmailValidator() {
-    return (String? value) {
-      if (value == "") {
+  static String? Function(String? value) nonMandatoryEmailValidator =
+      (String? value) {
+    if (value?.isNotEmpty == true) {
+      if (EmailValidator.validate(value!) == true) {
         return null;
-      } else if (!EmailValidator.validate(value!)) {
-        return "Enter a Valid Email";
+      } else {
+        return "Enter a valid email ID";
       }
-      return null;
-    };
-  }
+    }
+    return null;
+  };
 }
